fb14667c9ef3c1da619142c7f552817d88f7e815
gulsah <gmutluayyigit@gmail.com>
2024-04-02 01:05:28 +0300

    img

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,9 +1,22 @@
 
-
+import "./Main.css"
+import kedi from "./images/kedi.png"
 
 const Main = () => {
   return (
-    <div className="main">Main</div>
+    <div className="main">Main
+      <div>
+    <img className="img" src="https://ozenduvarkagidi.com.tr/images/urunler/Gokyuzu-Manzarasi-39-resim-10957.jpg" alt="manzara" />
+
+    <img className="img" src="https://ozenduvarkagidi.com.tr/images/urunler/Gokyuzu-Manzarasi-24-resim-10942.jpg" alt="manzara1" />
+
+    <img className="img" src={kedi} alt="kedi" />
+
+    <img className="img" src="./img/lessi.jpg" alt="lessi" />
+     
+      </div>
+    </div>
+    
   )
 }
 
